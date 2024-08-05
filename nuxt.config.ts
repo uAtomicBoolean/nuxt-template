6ab85671fbc339124cc309afb30adc0621f4be0d
AtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-08-05 15:35:18 +0200

    feat: template for Nuxt 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
 	compatibilityDate: '2024-07-07',
 
 	modules: [
-		'@nuxtjs/eslint-module',
+		'@nuxt/eslint',
 		'@nuxt/fonts',
 		'@nuxtjs/tailwindcss',
 		'@nuxt/icon',
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
 <template>
-	<p>Home</p>
+	<h1 class="text-2xl font-semibold">Main title</h1>
+	<p>Some random text.</p>
 </template>
